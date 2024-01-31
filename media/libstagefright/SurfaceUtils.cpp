03ee75a41461dc1eefb1bd2235324ddf4b4f176c
minaripenguin <minaripenguin@users.noreply.github.com>
2024-02-01 06:41:55 +0800

    Revert "libstagefright: use 64-bit usage for native_window_set_usage"
    This reverts commit d4c34dd9fae774e45a7d77a74b56cee389ad193b.

--- media/libstagefright/SurfaceUtils.cpp
+++ media/libstagefright/SurfaceUtils.cpp
@@ -111,9 +111,8 @@ status_t setNativeWindowSizeFormatAndUsage(
         }
     }
 
-    uint64_t finalUsage = (usage | consumerUsage) & 0xffffffffLL;
-    ALOGV("gralloc usage: %#x(producer) + %#x(consumer) = %#" PRIx64,
-            usage, consumerUsage, finalUsage);
+    int finalUsage = usage | consumerUsage;
+    ALOGV("gralloc usage: %#x(producer) + %#x(consumer) = %#x", usage, consumerUsage, finalUsage);
     err = native_window_set_usage(nativeWindow, finalUsage);
     if (err != NO_ERROR) {
         ALOGE("native_window_set_usage failed: %s (%d)", strerror(-err), -err);
@@ -127,7 +126,7 @@ status_t setNativeWindowSizeFormatAndUsage(
         return err;
     }
 
-    ALOGD("set up nativeWindow %p for %dx%d, color %#x, rotation %d, usage %#" PRIx64,
+    ALOGD("set up nativeWindow %p for %dx%d, color %#x, rotation %d, usage %#x",
             nativeWindow, width, height, format, rotation, finalUsage);
     return NO_ERROR;
 }
